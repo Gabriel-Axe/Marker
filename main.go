2d4a20d9b91daaead2493360a21a8db991eb770b
gabriel <gabrielaxesm@gmail.com>
2026-04-26 09:00:19 -0300

    feat: make the code call the user editor for editing

--- main.go
+++ main.go
@@ -3,15 +3,26 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 )
 
 func main() {
 	for _, arg := range(os.Args) {
 		fmt.Println(arg)
 	}
-	// if len(os.Args) < 2 {
-	// 	fmt.Println("no lol")
-	// 	return
-	// }
-	// fmt.Println("yes")
+
+	editor := os.Getenv("VISUAL")
+	if editor == "" {
+		editor = os.Getenv("EDITOR")
+	}
+	if editor == "" {
+		editor = "vi"
+	}
+
+	cmd := exec.Command(editor)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.Run()
 }
