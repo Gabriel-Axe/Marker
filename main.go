eebcb05a59c99b01a33a2d6808736930c4b0d770
gabriel <gabrielaxesm@gmail.com>
2026-04-26 09:15:10 -0300

    feat: make it create a new path with the file name

--- main.go
+++ main.go
@@ -7,9 +7,27 @@ import (
 )
 
 func main() {
-	for _, arg := range(os.Args) {
-		fmt.Println(arg)
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		fmt.Printf("Could not get current directory: %v", err)
 	}
+
+	if len(os.Args) != 3 {
+		fmt.Println("usage: k <path/to/template> <name>")
+		return
+	}
+
+	// templatePath := os.Args[1]
+	newNoteName := os.Args[2]
+
+	newNotePath := cwd + "/" + newNoteName + ".md"
+	fmt.Println(newNotePath)
+
+	// err = createFileFromTemplate(templatePath, newNoteName)
+	// if err != nil {
+	// 	fmt.Printf("Could not create file: %v", err)
+	// }
 
 	editor := os.Getenv("VISUAL")
 	if editor == "" {
@@ -19,10 +37,19 @@ func main() {
 		editor = "vi"
 	}
 
-	cmd := exec.Command(editor)
+	cmd := exec.Command(editor, )
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	cmd.Run()
+	// cmd.Run()
+}
+
+func createFileFromTemplate(templatePath, newPath string) error {
+	data, err := os.ReadFile(templatePath)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(newPath, data, 0644)
 }
