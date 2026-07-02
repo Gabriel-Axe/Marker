82d0cba7bd742024f66bc75029c7e1635205c0be
gabriel <gabrielaxesm@gmail.com>
2026-07-02 20:04:55 -0300

    refactor: insert a struct for notes and templates

--- main.go
+++ main.go
@@ -5,7 +5,17 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 )
+
+type Template struct {
+	Path string
+}
+
+type Note struct {
+	Path string
+	Name string
+}
 
 func main() {
 
@@ -14,22 +24,17 @@ func main() {
 		fmt.Printf("Could not get current directory: %v", err)
 	}
 
-	flag.Parse()
-	args := flag.Args()
-
-	if len(args) < 2 || len(args) > 3 {
-		fmt.Printf("usage: k <path/to/template> <name> [-open], number of args: %d\n", len(args))
-		os.Exit(1)
-	}
-
-	templatePath := os.Args[1]
-	newNoteName := os.Args[2]
-	var open bool = true
+	template, name := parseFlags()
 
 	newNotePath := cwd + "/" + newNoteName + ".md"
 	fmt.Println(newNotePath)
 
-	err = createFileFromTemplate(templatePath, newNotePath)
+	if strings.HasSuffix(name, ".md") {
+		name = strings.TrimSuffix(name, ".md")
+		print(name)
+	}
+
+	err = createFileFromTemplate(template, newNotePath)
 	if err != nil {
 		fmt.Printf("Could not create file: %v", err)
 	}
@@ -56,8 +61,24 @@ func main() {
 	}
 }
 
-func createFileFromTemplate(templatePath, newPath string) error {
-	data, err := os.ReadFile(templatePath)
+// Returns a struct rerpresenting a template and the name of the new note
+func parseFlags() (Template, string) {
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) < 2 || len(args) > 3 {
+		fmt.Printf("usage: k <path/to/template> <name> [-open], number of args: %d\n", len(args))
+		os.Exit(1)
+	}
+
+	template := Template { Path: os.Args[1] }
+	newNoteName := os.Args[2]
+
+	return template, newNoteName
+}
+
+func createFileFromTemplate(template Template, newPath string) error {
+	data, err := os.ReadFile(template.Path)
 	if err != nil {
 		return err
 	}
